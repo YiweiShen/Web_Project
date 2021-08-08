b73538a83fcb9412f88e73c628051d7ab59a1d81
XtC4 <xutongchen0@gmail.com>
2021-08-08 09:09:44 -0400

    commit

--- models/ordermodel.js
+++ models/ordermodel.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var orderSchema = new mongoose.Schema({
     orderId:{type: String},
-    name:{type: String},
+    userId:{type: String},
     movieName:{type: String},
     time:{type: String},
     phone:{type: String}
