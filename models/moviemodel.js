ccc3ef905385b6c3ace84860335aa8357f373d82
YiWeiShen <YiWeiShen@users.noreply.github.com>
2021-08-10 02:20:04 +0800

    fix booking page show movie and select movie

--- models/moviemodel.js
+++ models/moviemodel.js
@@ -11,3 +11,9 @@ var movieSchema = new mongoose.Schema({
 });
 
 mongoose.model('movie',movieSchema);
+
+// manually insert these records into the mongodb
+// MovieBookingDB
+// db.movies.insert({movieId: "1001", name:"The Shawshank Redemption", director:"Frank Darabont", length:"142 min", category: "Drama", year_released: "1994", price: 20})
+// db.movies.insert({movieId: "1002", name:"Another Movie", director:"Frank Smith", length:"133 min", category: "Music", year_released: "1999", price: 25})
+// db.movies.insert({movieId: "1003", name:"Third Movie", director:"John Darabont", length:"122 min", category: "Drama", year_released: "2002", price: 30})
